a582ffebe4138dadce6373d74e465e89f99fa7f0
bhagyagitrepo <bhagyashri.dhole24@gmail.com>
2022-08-01 13:34:27 +0530

    added LoginSteps.java

--- src/test/java/StepDefinitions/LoginSteps.java
+++ src/test/java/StepDefinitions/LoginSteps.java
@@ -10,25 +10,21 @@ public class LoginSteps {
 	@Given("user is on login page")
 	public void user_is_on_login_page() {
 		System.out.println("User inside login page");
-//		 throw new io.cucumber.java.PendingException();
 	}
 
 	@When("user enters username and password")
 	public void user_enters_username_and_password() {
 		System.out.println("User enters username ");
-//		 throw new io.cucumber.java.PendingException();
 	}
 
 	@And("click on the login button")
 	public void click_on_the_login_button() {
 		System.out.println("User clicm on login button");
-//		 throw new io.cucumber.java.PendingException();
 	}
 
 	@Then("user navigated to home page")
 	public void user_navigated_to_home_page() {
 		System.out.println("User navigates to Home page");
-//		 throw new io.cucumber.java.PendingException();
 	}
 
 }
