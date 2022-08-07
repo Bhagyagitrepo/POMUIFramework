9b4b2abd763268ba67010d11e7a0d520c0272b85
bhagyagitrepo <bhagyashri.dhole24@gmail.com>
2022-08-07 22:49:57 +0530

    added login.feature

--- src/test/java/StepDefinitions/LoginSteps.java
+++ src/test/java/StepDefinitions/LoginSteps.java
@@ -1,30 +1,71 @@
 package StepDefinitions;
 
-import io.cucumber.java.en.And;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class LoginSteps {
-
-	@Given("user is on login page")
-	public void user_is_on_login_page() {
+public class LoginSteps  {
+	
+	 WebDriver driver =null;
+	    
+	 @Given("user is on login page")
+	 public void user_is_on_login_page() throws InterruptedException{
+		
 		System.out.println("User inside login page");
+	    String projectPath=System.getProperty("user.dir");
+	    System.setProperty("webdriver.chrome.driver",projectPath+"/src/test/resources/drivers/chromedriver.exe");
+	   
+	    driver= new ChromeDriver();
+	    
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+        driver.manage().window().maximize();
+        driver.navigate().to("https://gmail.com");
+        driver.findElement(By.xpath(".//*[@type='email']")).click();
+		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
 	}
 
-	@When("user enters username and password")
-	public void user_enters_username_and_password() {
-		System.out.println("User enters username ");
-	}
+	 
+	 @When("user enters the username {string}")
+	 public void user_enters_the_username(String username) {
+		 driver.findElement(By.id("identifierId")).sendKeys(username);
+		 //driver.findElement(By.id("password")).sendKeys(password);
+	    
+	 }
+	 @When("click on the usenamenext button")
+	 public void click_on_the_usenamenext_button() {
+			//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+			//driver.findElement(By.xpath(".//*[@id='password']/div[1]/div/div[1]/input")).click();
+			driver.findElement(By.id("identifierNext")).click();
+		}
+	
+		
+	 @When("user enters password {string}")
+	 public void user_enters_password( String password) {
+		 driver.findElement(By.xpath(".//*[@id='password']/div[1]/div/div[1]/input")).sendKeys(password);
+		 //driver.findElement(By.id("password")).sendKeys(password);
+	    
+	 }
 
-	@And("click on the login button")
-	public void click_on_the_login_button() {
-		System.out.println("User clicm on login button");
+	@When("click on the passnext button")
+	public void click_on_the_passnext_button() {
+		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		//driver.findElement(By.xpath(".//*[@id='passwordNext']/div/button/span")).click();
+		driver.findElement(By.id("passwordNext")).click();
 	}
-
+	
+	
 	@Then("user navigated to home page")
-	public void user_navigated_to_home_page() {
+	public void user_navigated_to_home_page() throws InterruptedException {
 		System.out.println("User navigates to Home page");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.xpath(".//*[@id=\":1e\"]"));
 	}
 
 }
--- src/test/resources/Features/login.feature
+++ src/test/resources/Features/login.feature
@@ -1,20 +1,20 @@
 #Author 
 Feature: Feature to test login funvctionality
 
-@smoketest
-  Scenario: Verify the login is successful with valid credentials
-    Given user is on login page
-    When user enters username and password
-    And click on the login button
-    Then user navigated to home page
+
 @smoketest
   Scenario Outline: Verify the login is successful with valid credentials
     Given user is on login page
-    When user enters <username> and <password>
-    And click on the login button
+    When user enters the username "<username>" 
+    And click on the usenamenext button
+    When user enters password "<password>"
+    And click on the passnext button
     Then user navigated to home page
 
     Examples: 
       | username | password |
-      | user1    | pass1    |
-      | user2    | pass2    |
+      | bdtestautomationt@gmail.com| Happy123# |
+    
+
+      
+    
